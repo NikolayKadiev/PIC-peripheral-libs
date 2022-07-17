d51c2db67251bd3904af781ccd896e2b5a45dcdb
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2022-07-17 11:58:03 +0300

    Update uart.h

--- uart.h
+++ uart.h
@@ -7,6 +7,6 @@ void uart_send(uint8_t art_s);
 
 char uart_read(void);
 
-void uart_sendstr(char x[]);
+void uart_sendline(uint8_t *x, uint8_t len);
 
-void uart_sendstrnl(char x[]);
+void uart_sendstr(uint8_t *x);
